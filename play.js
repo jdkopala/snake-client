bb1d692105d75b38a145e6e611e8be75a2696faa
Jesse Kopala <jdkopala@gmail.com>
2022-06-08 10:06:52 -0600

    implemented some UI functions

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 const net = require('net');
 const connect = require('./client')
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+};
+
 console.log("Connecting ...");
+setupInput();
 connect();
