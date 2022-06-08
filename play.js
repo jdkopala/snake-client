d750a57828190b824ecc791c2cd8d1142273f520
Jesse Kopala <jdkopala@gmail.com>
2022-06-08 09:35:18 -0600

    refactoring files, added client.js which contains the net functions

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const net = require('net');
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.0.181',
-    port: 50541
-  })
-
-  conn.setEncoding('utf8');
-  // Display a success message when it connects
-  conn.on('connect', () => {
-    console.log('You\'ve connected!')
-  })
-  // Snarky message when you connect and timeout
-  conn.on('data', () => {
-    console.log('you ded cuz you idled')
-  });
-
-  return conn;
-};
+const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
