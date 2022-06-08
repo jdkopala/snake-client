56cc7700305db9e31ab57e55d9d62f8f14b5188f
Jesse Kopala <jdkopala@gmail.com>
2022-06-08 10:11:14 -0600

    Refactored input functions from play to input.js

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const net = require('net');
-const connect = require('./client')
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  const stdin = process.stdin;
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-};
+const { connect } = require('./client')
+const { setupInput } = require('./input')
 
 console.log("Connecting ...");
-setupInput();
 connect();
+
+setupInput();
